2e0149f3b5b62532dcb232c4edc4c74a1bd55a06
DevWankel <tariktanis2@gmail.com>
2022-07-06 19:50:38 +0300

    mer 4

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -10,5 +10,9 @@ public class Ogrenci {
         System.out.println("mer 1");
 
         System.out.println("mer 2");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("ROLEX");
+        }
     }
 }
